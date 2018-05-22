fc61e9cbed5ab41bab56642509e17393aa0bae6f
mjawath <mjawath@gmail.com>
2018-05-22 16:00:15 +0530

    fsfsdf

---
+++ src/app/sku/SKUView.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class SKUView extends Component {
+    render() {
+        return (
+            <div>
+                <SKUDetail>
+                </SKUDetail>
+                <SKUList>
+                    </SKUList>
+            </div>
+        );
+    }
+}
+
+export default SKUView;
